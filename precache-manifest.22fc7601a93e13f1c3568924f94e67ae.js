3fd93f7da8816c4453f9b1379eb1619c95e0ae7d
ToEndThePeace <bran.ramirez.don@gmail.com>
2020-04-18 14:47:27 -0700

    Updates

---
+++ precache-manifest.22fc7601a93e13f1c3568924f94e67ae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6a160369bbfecac9c794f22493c5092",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21a30cc122072dca7983",
+    "url": "/static/css/main.1fc25d6a.chunk.css"
+  },
+  {
+    "revision": "8b9569d930b1eb92372e",
+    "url": "/static/js/2.b1b2e277.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b1b2e277.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21a30cc122072dca7983",
+    "url": "/static/js/main.01bd21bf.chunk.js"
+  },
+  {
+    "revision": "4cd325359104d6a740d3",
+    "url": "/static/js/runtime-main.895b4d54.js"
+  },
+  {
+    "revision": "7e98bfcef67ba7635d02ca100e592336",
+    "url": "/static/media/PocketMonk-15ze.7e98bfce.ttf"
+  },
+  {
+    "revision": "8fb7702c55568cb5513d01538ccb2682",
+    "url": "/static/media/pk-bg.8fb7702c.jpg"
+  }
+]);
